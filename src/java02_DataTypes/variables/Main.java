0a63591ff742e96fc57616fead4b685ff29dd24d
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-02 16:28:04 +0530

    Add variables/Main.java

---
+++ src/java02_DataTypes/variables/Main.java
@@ -0,0 +1,16 @@
+package java02_DataTypes.variables;
+
+public class Main {
+	public static void main(String[] args) {
+		byte b = 50;
+		short s = 1996;
+		int i = 123456789;
+		long l = 12345654321L;
+		char c = 'A';
+		boolean boo = true;
+		float f = 3.14F;
+		double d = 0.0000000981;
+		System.out.println("Data Types & Variables types:");
+		System.out.println("b = "+b+"\ns = "+s+"\ni = "+i+"\nl = "+l+"\nc = "+c+"\nboo = "+boo+"\nf = "+f+"\nd = "+d+"\n");
+	}
+}
