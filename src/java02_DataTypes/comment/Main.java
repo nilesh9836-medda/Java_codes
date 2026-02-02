2dd567526e6f493c904c2a643efcec57d60b1cf7
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-02 16:27:25 +0530

    Add comment/Main.java

---
+++ src/java02_DataTypes/comment/Main.java
@@ -0,0 +1,19 @@
+// This is a single line comment
+
+/*
+* This is a multi line comment
+*/
+
+package java02_DataTypes.comment;
+
+public class Main {
+	public static void main(String[] args){
+		boolean comment = true;
+		//System.out.println("Comment it");
+		/*
+		if (comment) {
+			System.out.println("multi line comment");
+		}
+		*/
+	}
+}
