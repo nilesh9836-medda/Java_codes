e81e01e78c247926eb70587077c0b86395ee237b
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-08 03:08:16 +0530

    Add logical/ folder

---
+++ src/java04_operators/logical/Main.java
@@ -0,0 +1,16 @@
+package java04_operators.logical;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("Logical Operators: ||, &&, ~");
+    }
+}
