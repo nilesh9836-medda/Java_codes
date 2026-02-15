21300f336601c98998bad26d5697944ed4f297a5
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-15 22:23:01 +0530

    Add For Loop java program

---
+++ src/java06_Loop/loopFor/Main.java
@@ -0,0 +1,16 @@
+package src.java06_Loop.loopFor;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a positive number to calculate its factorial: ");
+        int n = sc.nextInt();
+        long fact = 1;
+        for (int i = 1; i <= n; ++i) {
+            fact *= i;
+        }
+        System.out.println("Factorial of "+n+" is = "+fact);
+    }
+}
